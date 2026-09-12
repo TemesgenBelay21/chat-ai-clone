e8355d06e882451e750af8fd1837d03eb702b677
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-12 21:51:21 +0100

    feat(backend): configure MySQL database connection pool and schema definition

---
+++ Backend/db/db.config.js
@@ -0,0 +1,15 @@
+
+import mysql2 from "mysql2/promise";
+
+const db = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
+});
+
+
+export default db;
