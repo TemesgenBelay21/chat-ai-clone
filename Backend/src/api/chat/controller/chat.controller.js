8a37a78dd5f16796b42a416484bb551268683a5f
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-15 21:10:09 +0100

    Revert "refactor(controller): remove redundant try/catch rethrows"
    This reverts commit c1a0060ecfefe92311372c1c2d409c12c60e9b2a.

--- Backend/src/api/chat/controller/chat.controller.js
+++ Backend/src/api/chat/controller/chat.controller.js
@@ -2,21 +2,31 @@ import {createConversationService,getRecentConversations } from "../service/chat
 
 
 async function createConversationController(req,res) {
-    const result = await createConversationService(req.body);
-    res.status(201).json({
-        status:true,
-        message: "message posted successfully" ,
-        data: result
-    })
+    try {
+        const result = await createConversationService(req.body);
+        res.status(201).json({
+            status:true,
+            message: "message posted successfully" ,
+            data: result
+        })
+    } catch(error) {
+        throw error;
+    }
+   
 }
 
 async function getConversationController(req,res) {
-    const result = await getRecentConversations(100);
-    res.status(200).json({
-        success: true,
-        message: "conversations fetched successfully",
-        data: result
-    })
+    try{
+          const result = await getRecentConversations(100);
+          res.status(200).json({
+            success: true,
+            message: "conversations fetched successfully",
+            data: result
+          })
+    }catch(error) {
+         throw error;
+    }
+   
 }
 
 
