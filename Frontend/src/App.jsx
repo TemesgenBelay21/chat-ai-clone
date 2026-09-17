be6de250695db7b3eafddaf94fd38a083e3f3e83
temesgenbelay21 <Temesgenyosobelay@gmail.com>
2026-09-17 15:57:40 +0100

    feat(frontend): fetch and render conversations in app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,31 @@
 import Sidebar from "./components/Sidebar/Sidebar";
 import Header from "./components/Header/Header";
+import MessageList from "./components/MessageList/MessageList";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [conversations, setConversations] = useState([]);
+
+  async function fetchConversations() {
+    try {
+      const { data } = await axios.get("http://localhost:3000/api/chat/conversations");
+
+      setConversations(data.data);
+      
+    } catch (error) {
+      console.error("Error fetching conversations:", error.message);
+    }
+  
+  }
+
+  useEffect(() => {
+    fetchConversations();
+  }, []);
+
+
+
   return (
     <div className="app">
       <Sidebar />
@@ -10,6 +34,8 @@ function App() {
         <Header />
 
         {/* conversation list */}
+
+        <MessageList conversations = {conversations} />
       </main>
     </div>
   );
